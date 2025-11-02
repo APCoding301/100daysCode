6ccb511f0db95c62eba092da99eae871b226bcf3
APCoding301 <buckvaasme@gmail.com>
2025-11-02 15:37:18 -0600

    if-elif added for weather condition "id" checks and emoji printing INSTEAD of Twilios SMS or whatsapp

--- day35.py
+++ day35.py
@@ -10,7 +10,8 @@ OWM_api_key = os.environ.get("OWM_API_KEY")
 #api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=be9709ae75673b58126078e9a00f88cc
 weather_params = {
     "q": "Guadalajara,Mexico",
-    "APPID": OWM_api_key
+    "APPID": OWM_api_key,
+    "units": "metric"
 }
 
 response = requests.get(OWM_endpoint, params=weather_params)
@@ -18,4 +19,14 @@ response.raise_for_status()
 weather_data = response.json()
 #print(response.status_code)
 #print(weather_data["weather"][0]["main"])
-print(weather_data)
+#print(weather_data)
+
+# For weather condition codes, URL is:
+# https://openweathermap.org/weather-conditions
+# These codes are sent in the JSON response of the API called...
+OWM_id_code = int(weather_data["weather"][0]["id"])
+if OWM_id_code >= 801:
+    print("Looks quite cloudy ☁️☁️☁️ !! You might want to bring an 🌂")
+elif OWM_id_code < 800:
+    print("Definitely raining, slushy, snowing OR thunderstorm! Bring a large and open ☔️ !!")
+
